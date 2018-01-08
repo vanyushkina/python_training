3551fe3512e75ed8653b09e85a3c21d5d12ac8af
Olga Vanyushkina <jilliya13@gmail.com>
2018-01-08 03:21:20 +0300

    Выделен класс для работы с группами

--- test/create_a_test_group.py
+++ test/create_a_test_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import pytest
-
 from fixture.application import Application
 from model.group import Group
 
@@ -14,11 +13,11 @@ def app(request):
 
 def test_test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test group", header="test", footer="create a test group"))
+    app.group.create(Group(name="test group", header="test", footer="create a test group"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
